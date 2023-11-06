4ed537af29751042dc8bb6286c0afb7639f332dc
QasimK <qasimworkaus@gmail.com>
2023-11-06 01:47:49 +0000

    TFRecord create added

--- data.py
+++ data.py
@@ -1,6 +1,3 @@
-import cv2
-
-
 def handle_tfds_protobuf_winerror(shuffle_py_dir):
     # Fix windows error for not including resource
     shuffle_py = shuffle_py_dir
@@ -29,6 +26,62 @@ except ImportError:
     print("Verify shuffle.py contents and re-run data.py")
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
+import sys
+import os
+import re
+
+
+def get_average_dir_file_size(directory, exclude_list=None, threshold=sys.float_info.max):
+    files = []
+    file_sizes = []
+    for file in os.listdir(directory):
+        if (exclude_list and file in exclude_list) or not file.__contains__('tfrecord'):
+            continue
+        files.append(file)
+    for i, file in enumerate(files):
+        filepath = os.path.join(directory, file)
+        size = os.path.getsize(filepath)
+        if (i + 1) != len(files):
+            next_filepath = os.path.join(directory, files[i + 1])
+            next_size = os.path.getsize(next_filepath)
+            if abs(size - next_size) <= threshold:
+                file_sizes.append(size)
+        else:
+            previous_filepath = os.path.join(directory, files[i - 1])
+            previous_size = os.path.getsize(previous_filepath)
+            if abs(size - previous_size) <= threshold:
+                file_sizes.append(size)
+    if not file_sizes:
+        return -1
+    average_dir_file_size = sum(file_sizes) / len(file_sizes)
+    return get_bytes_to_megabytes(average_dir_file_size)
+
+
+def extract_set_name_and_extension(file_path):
+    filename = os.path.basename(file_path)
+    name, extension = os.path.splitext(filename)
+    set_name = name.split('.')[0]
+    return set_name, extension
+
+
+def get_bytes_to_megabytes(bytes_size):
+    return bytes_size / (1024 * 1024)
+
+
+def rename_tfrecords(file_path, total_records):
+    directory, _ = os.path.split(file_path)
+    set_name, extension = extract_set_name_and_extension(file_path)
+    pattern = r'\-(\d+)-of-\d+'
+    match = re.search(pattern, extension)
+    if match:
+        index = int(match.group(1))
+    else:
+        return None
+    new_filename = f"{set_name}.tfrecord-{index:05d}-of-{total_records:05d}"
+    new_file_path = os.path.join(directory, new_filename)
+    os.rename(file_path, new_file_path)
+    print(f"File {file_path} renamed to {new_file_path}")
 
 
 class TFDataset:
@@ -40,11 +93,13 @@ class TFDataset:
         self.load_data(dataset_id)
 
     def load_data(self, dataset_id):
+        data_dir = os.path.join(os.curdir, 'dataset')
         (self.tfds_train_dataset, self.tfds_test_dataset), self.tfds_dataset_info = tfds.load(
             name=dataset_id,
             split=['train[:80%]', 'train[80%:]'],
             with_info=True,
             as_supervised=True,
+            data_dir=data_dir
         )
         print(self.tfds_dataset_info)
         print(f"{dataset_id} dataset loaded")
@@ -62,10 +117,7 @@ class TFDataset:
         return tfds.as_numpy(batch)  # Saved for expansion
 
     def map_label(self, label):
-        if label == 1:
-            return "Dog"
-        else:
-            return "Cat"
+        return self.tfds_dataset_info.features['label'].int2str(label)
 
     def view_batch(self, np_batch):
         images, labels = [], []
@@ -88,8 +140,37 @@ class TFDataset:
         plt.tight_layout()
         plt.show()
 
+    def create_tfrecord(self):
+        tfrecords = []
+        threshold_size = 10  # In megabytes
+        new_required = False
+        data_dir = self.tfds_dataset_info.data_dir
+        average_file_size = get_average_dir_file_size(data_dir)
+
+        for file in os.listdir(data_dir):
+            if file.__contains__('tfrecord'):
+                tfrecord_filepath = os.path.join(data_dir, file)
+                tfrecords.append(tfrecord_filepath)
+
+        last_size = get_bytes_to_megabytes(os.path.getsize(tfrecords[-2]))
+        if abs(last_size - average_file_size) <= threshold_size:
+            new_required = True
+
+        if new_required:
+            new_total = len(tfrecords) + 1
+            for tfrecord in tfrecords:
+                rename_tfrecords(tfrecord, new_total)
+            current_index = len(tfrecords)
+            set_name, extension = extract_set_name_and_extension(tfrecords[0])
+            tfrecord_filepath = os.path.join(data_dir, f"{set_name}.tfrecord-{current_index:05d}-of-{new_total:05d}")
+            tf.io.TFRecordWriter(tfrecord_filepath)
+            print(f"New tfrecord {tfrecord_filepath} created")
+        else:
+            print("New tfrecord not required")
+
 
 if '__main__' == __name__:
     dataset = TFDataset(dataset_id='cats_vs_dogs')
+    dataset.create_tfrecord()
     batch = dataset.get_batch(16, 'train')
     dataset.view_batch(batch)
